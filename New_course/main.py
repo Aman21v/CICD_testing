129a174b728c91694a9ca3eec36fe6551cde85cc
Aman21v <90607017+Aman21v@users.noreply.github.com>
2022-04-11 17:37:13 +0530

    Update main.py

--- New_course/main.py
+++ New_course/main.py
@@ -1,12 +1,12 @@
+import os
 from flask import Flask
-
+ 
 app = Flask(__name__)
-
-
+ 
 @app.route("/")
-def main():
-    return "Hello From Am"
-
-
+def hello_world():
+   name = os.environ.get("NAME", "World")
+   return "Hello {}!".format(name)
+ 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
