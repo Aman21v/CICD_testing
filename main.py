1fc8056cdd7f18be75df3f6fadf2d35df28f8392
Aman Verma <amanverma@C62GF9KN9D.local>
2022-04-11 15:45:42 +0530

    adding 3 files in pub repo

---
+++ main.py
@@ -0,0 +1,14 @@
+
+   
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Hello From Aman"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
